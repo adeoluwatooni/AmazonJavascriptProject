e21a0d81df50bc7c3a8f4c7060e2c584212f50df
adeoluwatooni <adeoluwa53@gmail.com>
2023-06-14 12:43:53 +0100

    dom manipulation

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,12 +1,65 @@
 
-let productsHTML =''
+// let productsHTML =''
 
-products.forEach((product) => {
-  productsHTML += `
+// products.forEach((product) => {
+//   productsHTML += `
+//   <div class="product-container">
+//     <div class="product-image-container">
+//       <img class="product-image"
+//         src="${product.image}">
+//     </div>
+
+//     <div class="product-name limit-text-to-2-lines">
+//       ${product.name}
+//     </div>
+
+//     <div class="product-rating-container">
+//       <img class="product-rating-stars"
+//         src="images/ratings/rating-${product.rating.stars *10}.png">
+//       <div class="product-rating-count link-primary">
+//         ${product.rating.count}
+//       </div>
+//     </div>
+
+//     <div class="product-price">
+//       $ ${(product.priceCents / 100).toFixed(2)}
+//     </div>
+
+//     <div class="product-quantity-container">
+//       <select>
+//         <option selected value="1">1</option>
+//         <option value="2">2</option>
+//         <option value="3">3</option>
+//         <option value="4">4</option>
+//         <option value="5">5</option>
+//         <option value="6">6</option>
+//         <option value="7">7</option>
+//         <option value="8">8</option>
+//         <option value="9">9</option>
+//         <option value="10">10</option>
+//       </select>
+//     </div>
+
+//     <div class="product-spacer"></div>
+
+//     <div class="added-to-cart">
+//       <img src="images/icons/checkmark.png">
+//       Added
+//     </div>
+
+//     <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
+//       Add to Cart
+//     </button>
+//   </div>`
+// })
+
+// document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+
+const productsHTML = products.map((product) => `
   <div class="product-container">
     <div class="product-image-container">
-      <img class="product-image"
-        src="${product.image}">
+      <img class="product-image" src="${product.image}">
     </div>
 
     <div class="product-name limit-text-to-2-lines">
@@ -14,8 +67,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-rating-container">
-      <img class="product-rating-stars"
-        src="images/ratings/rating-${product.rating.stars *10}.png">
+      <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars *10}.png">
       <div class="product-rating-count link-primary">
         ${product.rating.count}
       </div>
@@ -50,10 +102,9 @@ products.forEach((product) => {
     <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
       Add to Cart
     </button>
-  </div>`
-})
+  </div>`).join(' ')
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
 
